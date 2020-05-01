7453640e15d9055b3108a5f5010e7ca22a878e06
fpaez7 <fpaez1@uc.cl>
2020-04-30 20:18:27 -0400

    Logre cargar daros con las reuniones

--- leer_datos.py
+++ leer_datos.py
@@ -1,1 +1,16 @@
-askjdakls
+from collections import defaultdict
+import re
+
+
+
+""" Crea un diccionario donde las llaves son los dias y los elemntos son una
+lista con los sets con los id de las personas que se juntaron """
+
+data_reuniones = defaultdict(list)
+with open ("reuniones_1500.txt") as file:
+    linea = file.__next__()
+inter = linea.split("({")
+for i in range(1,len(inter)):
+    parcial = set(map(int,inter[i].split("}")[0].split(", ")))
+    dia = int(re.sub("[^0-9]", "",inter[i].split("}")[1] ))
+    data_reuniones[dia].append(parcial)
