f1213588d9915d33a5ab78a316253132cd0c2058
fpaez7 <fpaez1@uc.cl>
2020-05-09 19:54:48 -0400

    Cambios de enunciado y ordenamos las weas en carpetas

--- simulation_parameters.py
+++ simulation_parameters.py
@@ -4,12 +4,12 @@ from numpy.random import seed as npseed
 
 
 
-def simulation_parameters(total, s):
+def simulation_parameters(total, s, delta_t):
     seed(s)
     npseed(s)
     r_p = {i: poisson(15) for i in range(total)} # El tiempo de recuperacion
     c_p = {i: min(r_p[i], negative_binomial(6,0.5)) for i in range(total)} # Tiempo en que se da cuenta
-    contagios = {i: random() for i in range(total)}
+    contagios = {t: {i: random() for i in range(total)} for t in range(delta_t + 1)}
 
     return r_p, c_p, contagios
 
