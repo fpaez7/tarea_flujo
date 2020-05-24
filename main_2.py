727cefddba9c425d041d9d21a96fb9f5fe3c3689
fpaez7 <fpaez1@uc.cl>
2020-05-24 18:34:48 -0400

    Main_ ordenado para que sea la parte 1

--- main_2.py
+++ main_2.py
@@ -205,11 +205,11 @@ def resolver_pfmc(abastecimiento, encuentro, transporte, inventario) :
     with open('resultado.txt', 'w') as outfile:
         json.dump(flowDict, outfile)
     return flowDict
+if __name__ != '__main__':
+    grafico_pfmc()
 
 if __name__ == '__main__':
     resolver_pfmc("matriz_abastecimiento30x70.txt",
                     "matriz_encuentro50x70.txt",
                     "costos_transporte30x50.txt",
                     "costos_inventario30.txt")
-if __name__ != '__main__':
-    grafico_pfmc()
